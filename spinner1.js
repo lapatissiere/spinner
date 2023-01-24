9339890134331b9a20718b1cd0bef9f5e9b39030
Nazia Farooq <lapatissiere@yahoo.com>
2023-01-23 20:07:44 -0500

    spinner1 file created

---
+++ spinner1.js
@@ -0,0 +1,45 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+// ... fill in the rest yourself ...
+setTimeout(() => {
+  process.stdout.write('\r_   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r|/  ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r/-  ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r-\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\_   '); 
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r_|    ');
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r|_-\\  ');
+}, 2100);
